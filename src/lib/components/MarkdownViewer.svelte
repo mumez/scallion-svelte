86b7af8b0cf12739290b72d9f631b56cc19c2baf
Masashi Umezawa <ume@softumeya.com>
2023-03-01 00:08:59 +0900

    Added MarkdownViewer

---
+++ src/lib/components/MarkdownViewer.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import {marked} from 'marked';
+	export let markdown = '';
+	$: htmlText = marked(markdown);
+</script>
+
+<div class="space-x-0">
+	<article>{@html htmlText}</article>
+</div>
--- src/routes/wikis/[wiki]/[[page]]/+page.svelte
+++ src/routes/wikis/[wiki]/[[page]]/+page.svelte
@@ -4,6 +4,8 @@
 	import headerTitle from '$lib/stores/headerTitle';
 	import wikiPage from '$lib/stores/wikiPage';
 
+	import MarkdownViewer from '$lib/components/MarkdownViewer.svelte';
+
 	import type { PageData } from './$types';
 	export let data: PageData;
 
@@ -16,14 +18,23 @@
 
 	wikiPage.setPageContent(pageContent);
 
-	$: buttonLabel = $wikiPage.isEditing ? 'Save' : 'Edit';
+	let editingContent = pageContent.content;
+	//setInterval(() => {editingContent = new Date().toString()},1000)
 
+	$: buttonLabel = $wikiPage.isEditing ? 'Save' : 'Edit';
+	
 </script>
 
 <div class="container mx-auto p-4 space-y-4">
-	<section class="border-solid border-2 p-4">{pageContent.content}</section>
-
-	<p>Last update: {new Date(pageContent.updatedAt)}</p>
+	{#if $wikiPage.isEditing}
+		<div class="grid gap-4 grid-cols-2">
+			<textarea bind:value={editingContent}/>
+			<MarkdownViewer markdown="{editingContent}"></MarkdownViewer>
+		</div>
+	{:else}
+		<section class="border-solid border-2 p-4">{pageContent.content}</section>
+		<p>Last update: {new Date(pageContent.updatedAt)}</p>
+	{/if}
 	<hr />
 	<section class="flex space-x-2">
 		<button class="btn variant-filled-primary" on:click={wikiPage.toggleEditing}>{buttonLabel}</button>
