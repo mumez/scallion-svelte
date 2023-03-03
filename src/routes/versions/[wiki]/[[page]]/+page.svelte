50f6f7cf0558a4d6f342e1fef2ac2d4818d39f85
Masashi Umezawa <ume@softumeya.com>
2023-03-03 16:42:45 +0900

    Implemented simple versions table

--- src/routes/versions/[wiki]/[[page]]/+page.svelte
+++ src/routes/versions/[wiki]/[[page]]/+page.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
 	import { page } from '$app/stores';
+	import { Table, Paginator, tableSourceMapper, tableSourceValues } from '@skeletonlabs/skeleton';
+	import type { PaginationSettings } from '@skeletonlabs/skeleton/components/Paginator/types';
 	import parentLink from '$lib/stores/parentLink';
 	import headerTitle from '$lib/stores/headerTitle';
+	import VersionsService from '$lib/services/VersionsService';
+	import type {PageContent} from '$lib/models/PageContent';
+	
 	import type { PageData } from './$types';
-	import { Table, Paginator } from '@skeletonlabs/skeleton';
 	export let data: PageData;
 
 	let lastVersionNumber = data.lastVersionNumber;
@@ -11,31 +15,21 @@
 	let wikiName = $page.params['wiki'] ?? '';
 	let pageName = $page.params['page'] ?? 'index';
 
+	const versionsService = new VersionsService(wikiName, pageName);
+
 	$parentLink = wikiName;
 	$headerTitle = pageName;
 
 	let versionFrom = lastVersionNumber;
-	let rowsPerPage = 2;
+	let rowsPerPage = 5;
 
-	const versionsHeaders: string[] = ['Positions', 'Name', 'Weight', 'Symbol'];
-	const sourceBody: any = [
-		[1, 'Hydrogen', 1.0079, 'H'],
-		[2, 'Helium', 4.0026, 'He'],
-		[3, 'Lithium', 6.941, 'Li'],
-		[4, 'Beryllium', 9.0122, 'Be'],
-		[5, 'Boron', 10.811, 'B'],
-		[6, 'Carbon', 12.0107, 'C'],
-		[7, 'Nitrogen', 14.0067, 'N'],
-		[8, 'Oxygen', 15.9994, 'O'],
-		[9, 'Fluorine', 18.9984, 'F'],
-		[10, 'Neon', 20.1797, 'Ne']
-	];
+	const versionsTableHeaders: string[] = ['Version', 'Content', 'Updater', 'Date'];
 
 	let pagination = {
 		offset: 0,
 		limit: rowsPerPage,
 		size: versionFrom,
-		amounts: [2, 5]
+		amounts: [5, 10]
 	} as PaginationSettings;
 
 	function onPageChange(e: CustomEvent): void {
@@ -48,27 +42,30 @@
 		rowsPerPage = e.detail;
 		getVersions();
 	}
-	function getVersions(){
-		console.log('getVersions:', versionFrom, rowsPerPage)
+	async function getVersions(){
+		versions = await versionsService.getVersions(versionFrom, rowsPerPage);
+		console.log('getVersions:', versions);
+	}
+	function processVersionsForTable(versions: PageContent[]){
+		let mappedVersions = tableSourceMapper(versions, ['content','updatedBy','updatedAt']);
+		mappedVersions = mappedVersions.map((v, i) => {
+			v.updatedAt = new Date(v.updatedAt);
+			return {
+				version: versionFrom - i,
+				...v
+			}
+		});
+		return tableSourceValues(mappedVersions);
 	}
 
-	$: sourceBodySliced = sourceBody.slice(
-		pagination.offset * pagination.limit,
-		pagination.offset * pagination.limit + pagination.limit
-	);
+	$: versionsTableBody = processVersionsForTable(versions);
 </script>
 
 <div class="container mx-auto p-4 space-y-4">
-	{wikiName}{pageName}
-	<div>offset:{pagination.offset}, limit:{pagination.limit}</div>
-	{#each versions as version}
-		<li>{version.updatedAt}: {version.content}</li>
-	{/each}
-
 	<Table
 		source={{
-			head: versionsHeaders,
-			body: sourceBodySliced
+			head: versionsTableHeaders,
+			body: versionsTableBody
 		}}
 	/>
 	<Paginator bind:settings={pagination} on:page={onPageChange} on:amount={onAmountChange} />
--- src/routes/versions/[wiki]/[[page]]/+page.ts
+++ src/routes/versions/[wiki]/[[page]]/+page.ts
@@ -4,7 +4,7 @@ export async function load({ params }) {
 	console.log('-params--', params);
 	const versionsService = new VersionsService(params.wiki, params.page ?? 'index');
 	const lastVersionNumber = await versionsService.getLastVersionNumber();
-	const versions = await versionsService.getVersions(lastVersionNumber, 2);
+	const versions = await versionsService.getVersions(lastVersionNumber, 5);
 	console.log('lastVersionNumber :>> ', lastVersionNumber, versions);
 	return { lastVersionNumber, versions };
 }
