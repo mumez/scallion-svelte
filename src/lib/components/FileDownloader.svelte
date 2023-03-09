d04b6163c3225a11d89aaa4944deb3ca1aeb1b52
Masashi Umezawa <ume@softumeya.com>
2023-03-10 00:17:07 +0900

    Fixed layout of FileDownloader

--- src/lib/components/FileDownloader.svelte
+++ src/lib/components/FileDownloader.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { Avatar } from '@skeletonlabs/skeleton'; 
+	import { Avatar } from '@skeletonlabs/skeleton';
 	import ModalCloseButton from '$lib/components/ModalCloseButton.svelte';
 
 	export let fileName = '';
@@ -7,11 +7,11 @@
 	export let parent: unknown;
 
 	function isImage() {
-		return (/\.(gif|jpe?g|tiff?|png|webp|bmp)$/i).test(fileName);
+		return /\.(gif|jpe?g|tiff?|png|webp|bmp)$/i.test(fileName);
 	}
 
 	function fileExtension() {
-		return fileName.slice((Math.max(0, fileName.lastIndexOf(".")) || Infinity) + 1);
+		return fileName.slice((Math.max(0, fileName.lastIndexOf('.')) || Infinity) + 1);
 	}
 
 	async function download() {
@@ -25,20 +25,20 @@
 		document.body.appendChild(a);
 		a.click();
 		window.URL.revokeObjectURL(url);
-
-		console.log('parent :>> ', parent);
 	}
 </script>
 
 <div class="relative">
 	<ModalCloseButton />
-	{#if isImage()}
-		<Avatar src="{baseUrl}/{fileName}" rounded="rounded-xl"/>
-	{:else}
-		<div class="card w-12 h-12 rounded-xl bg-white text-center truncate">{fileExtension()}</div>
-	{/if}
-	<div>
-		<a href="{baseUrl}/{fileName}" on:click|preventDefault={download}>{fileName}</a>
+	<div class="flex items-center">
+		{#if isImage()}
+			<Avatar src="{baseUrl}/{fileName}" rounded="rounded-xl" />
+		{:else}
+			<div class="card w-12 h-12 rounded-xl bg-white text-center truncate">{fileExtension()}</div>
+		{/if}
+		<div class="p-2 truncate">
+			<a href="{baseUrl}/{fileName}" on:click|preventDefault={download}>{fileName}</a>
+		</div>
 	</div>
 	<slot />
 </div>
