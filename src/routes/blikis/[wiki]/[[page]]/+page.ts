76da4dd13b13e131ee0cea01773664b4023d5a86
Masashi Umezawa <ume@softumeya.com>
2023-05-01 17:32:08 +0900

    Adding blikis page support

---
+++ src/routes/blikis/[wiki]/[[page]]/+page.ts
@@ -0,0 +1,29 @@
+import PageService from '$lib/services/PageService';
+import { newPageContent } from '$lib/models/PageContent';
+import UpdatesService from '$lib/services/UpdatesService';
+
+export async function load({ params, fetch }) {
+	const wikiBookName = params.wiki;
+	const pageName = params.page;
+	if (pageName) {
+		return await loadPage(wikiBookName, pageName, fetch);
+	}
+	return await loadUpdates(wikiBookName, fetch);
+}
+
+async function loadUpdates(wikiBookName: string, fetch: (input: RequestInfo | URL, init?: RequestInit | undefined) => Promise<Response>) {
+	const updatesService = new UpdatesService(wikiBookName);
+	updatesService.fetcher(fetch);
+	const pages = await updatesService.getUpdates();
+	return { pages };
+}
+
+async function loadPage(wikiBookName: string, pageName: string, fetch: (input: RequestInfo | URL, init?: RequestInit | undefined) => Promise<Response>) {
+	const pageService = new PageService(wikiBookName, pageName);
+	pageService.fetcher(fetch);
+	let page = await pageService.getContent();
+	if (!page.id) {
+		page = newPageContent(wikiBookName, pageName, '');
+	}
+	return { page };
+}
