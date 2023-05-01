94520ef67f453fa3db2677bdeedd048d232d16f2
Masashi Umezawa <ume@softumeya.com>
2023-05-01 17:31:40 +0900

    Added UpdatesService

---
+++ src/lib/services/UpdatesService.ts
@@ -0,0 +1,31 @@
+import BaseApiService from './BaseApiService';
+import type { PageContent } from '$lib/models/PageContent';
+
+class UpdatesService extends BaseApiService {
+	protected wikiName = '';
+
+	constructor(wikiName: string) {
+		super();
+		this.wikiName = wikiName;
+	}
+
+	public async getUpdates(from = 1, size = 1): Promise<PageContent[]> {
+		const url = `${this.targetUrl()}&from=${from}&size=${size}`;
+		const resp = await this.apiAccessor.get(url).catch(() => {
+			return [];
+		});
+		return resp as PageContent[];
+	}
+
+	override get serviceName() {
+		return 'updates';
+	}
+
+	// accessing
+	targetUrl() {
+		return `${this.serviceName}?wiki=${this.wikiName}`;
+	}
+
+}
+
+export default UpdatesService;
