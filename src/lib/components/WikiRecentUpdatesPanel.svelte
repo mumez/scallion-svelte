02d2aaf4ba440cd66e6832b848f4baf85017fd84
Masashi Umezawa <ume@softumeya.com>
2023-05-08 16:17:39 +0900

    WikiRecentUpdatesPanel aggregates the most recent updates by date

---
+++ src/lib/components/WikiRecentUpdatesPanel.svelte
@@ -0,0 +1,81 @@
+<script lang="ts">
+	import { groupBy, mapValues } from 'lodash-es';
+	import { page } from '$app/stores';
+	import { getLatestUpdates } from '$lib/utils/CoreUtils.js';
+	import MarkdownViewer from '$lib/components/MarkdownViewer.svelte';
+	import FilesService from '$lib/services/FilesService';
+	import UpdatesService from '$lib/services/UpdatesService';
+	import type { PageContent } from '$lib/models/PageContent';
+
+	export let pages: PageContent[];
+
+	const wikiName = $page.params['wiki'] ?? '';
+	const wikiBasePart = ($page.route.id ?? '').split('/')[1];
+	let allLoaded = false;
+
+	const updatesService = new UpdatesService(wikiName);
+
+	function attachmentsBaseUrlFor(pageName: string) {
+		const filesService = new FilesService(wikiName, pageName);
+		return filesService.downloadBaseUrl;
+	}
+
+	function dateRoundedValueFor(updatedAt: number) {
+		const date = new Date(updatedAt);
+		date.setHours(0, 0, 0, 0);
+		return date.valueOf();
+	}
+
+	function localeDateStringFor(updatedAt: number) {
+		return new Date(updatedAt).toLocaleDateString();
+	}
+	function localeDateTimeStringFor(updatedAt: number) {
+		return new Date(updatedAt).toLocaleString();
+	}
+
+	async function loadMore() {
+		const morePages = await updatesService.getUpdates(oldestUpdatedAt, 10);
+		if (morePages.length == 0) {
+			allLoaded = true;
+		}
+		pages = pages.concat(morePages);
+		console.log('pages :>> ', pages);
+		console.log('morePages :>> ', morePages);
+		console.log('groupedPages :>> ', groupedPages);
+	}
+
+	function groupedPagesFrom(pages: PageContent[]){
+		let groups = groupBy(pages, (page) => dateRoundedValueFor(page.updatedAt));
+		groups = mapValues(groups, (group) => getLatestUpdates<PageContent>(group, 'name', 'updatedAt'));
+		return Object.entries(groups).sort((a, b) => parseInt(b[0]) - parseInt(a[0]));
+	}
+
+	$: oldestUpdatedAt = pages[pages.length - 1]?.updatedAt ?? 0;
+	$: groupedPages = groupedPagesFrom(pages);
+</script>
+
+<div class="container mx-auto p-4 space-y-4 swiki-{wikiName.toLowerCase()}">
+	{#each groupedPages as grouped }
+		<h2 class="text-4xl">{localeDateStringFor(parseInt(grouped[0]))}</h2>
+		{#each grouped[1] as page}
+			<div class="flex flex-col sm:flex-row justify-between">
+				<div class="text-3xl sm:text-left mb-4 sm:mb-0">{page.name}</div>
+				<div class="sm:text-right">{page.updatedBy}</div>
+			</div>
+			<MarkdownViewer
+				markdown={page.content}
+				{wikiName}
+				{wikiBasePart}
+				attachmentsBaseUrl={attachmentsBaseUrlFor(page.name)}
+			/>
+			<div class="text-right">{localeDateTimeStringFor(page.updatedAt)}</div>
+		{/each}
+	{/each}
+	<div class="h-4" />
+	<div class="flex justify-end">
+		<button disabled={allLoaded} type="button" class="btn variant-ringed" on:click={loadMore}>
+			<span>More...</span>
+			<i class="fa-solid fa-angles-down" />
+		</button>
+	</div>
+</div>
