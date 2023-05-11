b5da1369478df00512a4f0f88023deed79269696
Masashi Umezawa <ume@softumeya.com>
2023-05-11 16:33:38 +0900

    Changed bliki page initial load function to load more updates

--- src/routes/blikis/[wiki]/[[page]]/+page.ts
+++ src/routes/blikis/[wiki]/[[page]]/+page.ts
@@ -18,8 +18,21 @@ async function loadUpdates(
 ) {
 	const updatesService = new UpdatesService(wikiBookName);
 	updatesService.fetcher(fetch);
-	const pages = await updatesService.getUpdates(Date.now(), 15);
-	const latestUpdates = getLatestUpdates<PageContent>(pages, 'name', 'updatedAt');
+	const from = Date.now() + (60 * 1000);
+	const maxUpdates = 10;
+	const chunkSize = 15;
+
+	let pages = await updatesService.getUpdates(from, chunkSize);
+	let latestUpdates = getLatestUpdates<PageContent>(pages, 'name', 'updatedAt');
+
+	while (pages.length > 0 && latestUpdates.length < maxUpdates) {
+		const lastItem = pages[pages.length - 1];
+		const lastUpdatedAt = lastItem?.updatedAt;
+		pages = await updatesService.getUpdates(lastUpdatedAt, chunkSize);
+		const moreLatestUpdates = getLatestUpdates<PageContent>(pages, 'name', 'updatedAt');
+		latestUpdates = latestUpdates.concat(moreLatestUpdates);
+	}
+
 	return { pages: latestUpdates };
 }
 
