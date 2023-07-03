80f2da8a0a6f92e00ff281984ce06ef69d3c58fc
Masashi Umezawa <ume@softumeya.com>
2023-07-03 14:10:22 +0900

    Changed to use normal functions

--- src/lib/utils/MarkdownHandler.ts
+++ src/lib/utils/MarkdownHandler.ts
@@ -6,20 +6,20 @@ import highlighter from '$lib/plugins/highlight';
 
 const defaultOptions = { mangle: false, headerIds: false };
 
-const isInternalPageLink = (href: string): boolean => {
+function isInternalPageLink(href: string): boolean {
 	return isInternalLink(href) && !isImageFileLink(href);
-};
+}
 
-export const isInternalLink = (href: string): boolean => {
+export function isInternalLink(href: string): boolean {
 	return /^[^/]+$/.test(href);
-};
+}
 
-export const isImageFileLink = (href: string): boolean => {
+export function isImageFileLink(href: string): boolean {
 	const regex = /\.(jpg|jpeg|gif|png|svg)$/i;
 	return regex.test(href);
-};
+}
 
-const renderLink = (
+function renderLink(
 	href: string,
 	text: string,
 	wikiName: string,
@@ -30,7 +30,7 @@ const renderLink = (
 		isAttachmentOnly: false,
 		newPageLinkTitle: ''
 	}
-): string => {
+): string {
 	const linkRenderer = new LinkRenderer(
 		wikiName,
 		existingPageNames,
@@ -41,27 +41,27 @@ const renderLink = (
 	return context.isAttachmentOnly
 		? linkRenderer.renderForAttachment(href, text)
 		: linkRenderer.render(href, text);
-};
+}
 
-const renderCodeBlock = (code: string, language: string): string => {
+function renderCodeBlock(code: string, language: string): string {
 	const detectedLanguage = highlighter.getLanguage(language);
 	const validLanguage = detectedLanguage?.name ?? 'plaintext';
 	const highlightedCode = highlighter.highlightAuto(code, [validLanguage]).value;
 	return `<pre><code class="hljs ${validLanguage}">${highlightedCode}</code></pre>`;
-};
+}
 
-export const htmlFrom = (markdown: string): string => {
+export function htmlFrom(markdown: string): string {
 	return sanitizedHtmlFrom(markdown);
-};
+}
 
-export const enrichedHtmlFrom = (
+export function enrichedHtmlFrom(
 	markdown: string,
 	wikiName: string,
 	existingPageNames: string[] = [],
 	wikisBaseDirectory = '',
 	attachmentsBaseUrl = '',
 	newPageLinkTitle = ''
-): string => {
+): string {
 	const renderer = {
 		code(code: string, language: string) {
 			return renderCodeBlock(code, language);
@@ -86,13 +86,13 @@ export const enrichedHtmlFrom = (
 	const options = { renderer, ...defaultOptions };
 	marked.use(options);
 	return sanitizedHtmlFrom(markdown);
-};
+}
 
-export const sanitizedHtmlFrom = (markdown: string): string => {
+export function sanitizedHtmlFrom(markdown: string): string {
 	return sanitize(marked.parse(markdown));
-};
+}
 
-export const extractInternalPageLinks = (markdown: string): string[] => {
+export function extractInternalPageLinks(markdown: string): string[] {
 	const internalLinks: string[] = [];
 	const tokens = marked.lexer(markdown);
 	marked.walkTokens(tokens, (token) => {
@@ -101,4 +101,4 @@ export const extractInternalPageLinks = (markdown: string): string[] => {
 		}
 	});
 	return internalLinks;
-};
+}
